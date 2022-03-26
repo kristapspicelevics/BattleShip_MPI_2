b5a5ae4e7ea6f653969a90f298802dee99740ff6
kristapspicelevics <64955441+kristapspicelevics@users.noreply.github.com>
2022-03-26 11:36:00 +0200

    Added Images

--- app/src/main/java/lv/kristaps/battleship/Adapter.java
+++ app/src/main/java/lv/kristaps/battleship/Adapter.java
@@ -4,17 +4,21 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Adapter extends BaseAdapter {
 
     private final Context mContext;
     private final int[] playerMap;
+    private final int[] imageid;
 
     // 1
-    public Adapter(Context context, int[] playerMap) {
+    public Adapter(Context context, int[] playerMap, int[] Imageid) {
         this.mContext = context;
         this.playerMap = playerMap;
+        this.imageid = Imageid;
     }
 
     // 2
@@ -38,8 +42,64 @@ public class Adapter extends BaseAdapter {
     // 5
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+//        ImageView dummyImgView = new ImageView(mContext);
+//        ImageView imageView = (ImageView);
+//        View gridView;
+//        gridView.findViewById(R.id.grid_item_image);
+//        dummyImgView.setImageResource(imageid[position]);
         TextView dummyTextView = new TextView(mContext);
         dummyTextView.setText(String.valueOf(playerMap[position]));
-        return dummyTextView;
+//        return dummyImgView;
+        ImageView mImageView;
+
+        if (convertView == null) {
+            mImageView = new ImageView(mContext);
+            mImageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+            mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            mImageView.setPadding(0, 0, 0, 0);
+        } else {
+            mImageView = (ImageView) convertView;
+        }
+        switch (playerMap[position]){
+            case -3:
+                mImageView.setImageResource(imageid[2]);
+                break;
+            case -1:
+                mImageView.setImageResource(imageid[26]);
+                break;
+            case 0:
+                mImageView.setImageResource(imageid[0]);
+                break;
+            case 1:
+                mImageView.setImageResource(imageid[0]);
+                break;
+            case 2:
+                mImageView.setImageResource(imageid[5]);
+                break;
+            case 4:
+                mImageView.setImageResource(imageid[7]);
+                break;
+            case 5:
+                mImageView.setImageResource(imageid[13]);
+                break;
+            case 6:
+                mImageView.setImageResource(imageid[3]);
+                break;
+            case 8:
+                mImageView.setImageResource(imageid[9]);
+                break;
+            case 45:
+                mImageView.setImageResource(imageid[11]);
+                break;
+            case 85:
+                mImageView.setImageResource(imageid[12]);
+                break;
+            case 99:
+                mImageView.setImageResource(imageid[1]);
+                break;
+            default:
+        }
+
+        return mImageView;
     }
 }
--- app/src/main/java/lv/kristaps/battleship/MainActivity.java
+++ app/src/main/java/lv/kristaps/battleship/MainActivity.java
@@ -14,12 +14,46 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     GridView gridView;
     static int[] playerMap = new int [100];
+    //static int[] mapIndex = new int [100];
     TextView text;
     Button button;
     Adapter adapter;
+    int gridIndex;
+    int[] imageId = {
+            R.drawable.blue, //0
+            R.drawable.fire_in_water,
+            R.drawable.missed_shot,
+            R.drawable.pakala_hor_kreisi,
+            R.drawable.pakala_hor_labi,
+            R.drawable.pakala_ver_augsu, // 5
+            R.drawable.pakala_ver_leju,
+            R.drawable.prieksa_hor_kreisi,
+            R.drawable.prieksa_hor_labi,
+            R.drawable.prieksa_ver_augsu,
+            R.drawable.prieksa_ver_leju, // 10
+            R.drawable.vidus_hor,
+            R.drawable.vidus_ver,
+            R.drawable.vieninieks,
+            R.drawable.back_hor_kreisi_dead,
+            R.drawable.back_hor_labi_dead, // 15
+            R.drawable.back_vert_augsu_dead,
+            R.drawable.back_vert_leju_dead,
+            R.drawable.front_hor_kreisi_dead,
+            R.drawable.front_hor_labi_dead,
+            R.drawable.front_vert_augsu_dead, // 20
+            R.drawable.front_vert_leju_dead,
+            R.drawable.midle_hor_kreisi_dead,
+            R.drawable.midle_hor_labi_dead,
+            R.drawable.midle_ver_augsu_dead,
+            R.drawable.midle_ver_leju_dead, // 25
+            R.drawable.vieninieks_dead,
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+//        for (Integer i=0; i<100; i++){
+//            mapIndex[i]=i;
+//        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Generator.populateMap(playerMap);
@@ -33,14 +67,43 @@ public class MainActivity extends AppCompatActivity {
                 Generator.populateMap(playerMap);
                 Generator.generateMap(playerMap);
 
-                adapter = new Adapter(MainActivity.this, playerMap);
+                adapter = new Adapter(MainActivity.this, playerMap, imageId);
                 gridView.setAdapter(adapter);
             }
         });
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 Toast.makeText(MainActivity.this, "" + playerMap[position], Toast.LENGTH_SHORT).show();
-                playerMap[position] = -1;
+
+                gridIndex = position;//saglabā to vērtību, kas ir pirms šaviena
+//
+                //VAJAG PADOT LĪDZI TO KEYVALUE KAUT KĀ, JO PATREIZ IR PIEEJAMS TIKAI TAS CIPARS, KAS IR
+                //ATTIECĪGAJĀ KVADRANTĀ
+
+                //-3 apzīmē aizšautu garām lauku
+                //-1 apzīmē nogrimušu kuģi
+                //99 apzīmē ievainotu kuģi
+                if (playerMap[position] == 1 || playerMap[position] == 0) {
+                    playerMap[position] = -3; // ja ir 1 vai 0, tad tur bus garām aizsauts un būs udens
+                }else if(playerMap[position] == -1 || playerMap[position] == 99 || playerMap[position] == -3 ){ //šauj pa lauku, kuram jau ir trāpīts
+                    //tad netiek netekas mainīts un atkārtoti var šaut, jo divreiz pa vienu un to pašu lauku nevar šaut
+                }else if (playerMap[position] == 5){ //kuģis grimst
+                    playerMap[position] = -1;
+                }else {
+                    playerMap[position] = 99;
+                    //isSunk();
+                }
+                switch (playerMap[position]){
+                    case 1:
+                        gridIndex = playerMap[position];//saglabā to vērtību, kas bija iepriekš
+                        playerMap[position] = -1;// ja ir 1 vai 0, tad tur bus garām aizsauts un būs udens
+                        break;
+                    case 45:
+                        playerMap[position] = 99;
+                        break;
+                    default:
+                        //tad netiek netekas mainīts un atkārtoti var šaut, jo divreiz pa vienu un to pašu lauku nevar šaut
+                }
                 gridView.setAdapter(adapter);
             }
         });
