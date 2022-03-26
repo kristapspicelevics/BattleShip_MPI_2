9bd9c199a1304b2d334d126dad9daca82c0c4e43
kristapspicelevics <64955441+kristapspicelevics@users.noreply.github.com>
2022-03-26 13:16:17 +0200

    Added disable grid element of click

--- app/src/main/java/lv/kristaps/battleship/Adapter.java
+++ app/src/main/java/lv/kristaps/battleship/Adapter.java
@@ -40,66 +40,91 @@ public class Adapter extends BaseAdapter {
     }
 
     // 5
+
+
+    public boolean isEnabled(int position) {
+        if (playerMap[position] == -3 || playerMap[position] == -1 || playerMap[position] == 99) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-//        ImageView dummyImgView = new ImageView(mContext);
-//        ImageView imageView = (ImageView);
-//        View gridView;
-//        gridView.findViewById(R.id.grid_item_image);
-//        dummyImgView.setImageResource(imageid[position]);
-        TextView dummyTextView = new TextView(mContext);
-        dummyTextView.setText(String.valueOf(playerMap[position]));
-//        return dummyImgView;
         ImageView mImageView;
-
         if (convertView == null) {
             mImageView = new ImageView(mContext);
-            mImageView.setLayoutParams(new GridView.LayoutParams(100, 100));
+            mImageView.setLayoutParams(new GridView.LayoutParams(110, 100));
             mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             mImageView.setPadding(0, 0, 0, 0);
         } else {
             mImageView = (ImageView) convertView;
         }
-        switch (playerMap[position]){
-            case -3:
-                mImageView.setImageResource(imageid[2]);
-                break;
-            case -1:
-                mImageView.setImageResource(imageid[26]);
-                break;
-            case 0:
-                mImageView.setImageResource(imageid[0]);
-                break;
-            case 1:
-                mImageView.setImageResource(imageid[0]);
-                break;
-            case 2:
-                mImageView.setImageResource(imageid[5]);
-                break;
-            case 4:
-                mImageView.setImageResource(imageid[7]);
-                break;
-            case 5:
-                mImageView.setImageResource(imageid[13]);
-                break;
-            case 6:
-                mImageView.setImageResource(imageid[3]);
-                break;
-            case 8:
-                mImageView.setImageResource(imageid[9]);
-                break;
-            case 45:
-                mImageView.setImageResource(imageid[11]);
-                break;
-            case 85:
-                mImageView.setImageResource(imageid[12]);
-                break;
-            case 99:
-                mImageView.setImageResource(imageid[1]);
-                break;
-            default:
-        }
+    //    mImageView = showPlayerImages(playerMap,mImageView,position);
+        mImageView = showImages(playerMap,-3,mImageView,imageid[2],position);
+        mImageView = showImages(playerMap,-1,mImageView,imageid[26],position);
+        mImageView = showImages(playerMap,0,mImageView,imageid[0],position);
+        mImageView = showImages(playerMap,1,mImageView,imageid[0],position);
+        mImageView = showImages(playerMap,2,mImageView,imageid[5],position);
+        mImageView = showImages(playerMap,4,mImageView,imageid[7],position);
+        mImageView = showImages(playerMap,5,mImageView,imageid[13],position);
+        mImageView = showImages(playerMap,6,mImageView,imageid[3],position);
+        mImageView = showImages(playerMap,8,mImageView,imageid[9],position);
+        mImageView = showImages(playerMap,45,mImageView,imageid[11],position);
+        mImageView = showImages(playerMap,85,mImageView,imageid[12],position);
+        mImageView = showImages(playerMap,99,mImageView,imageid[1],position);
 
         return mImageView;
     }
+
+    private ImageView showImages(int[] map, int value, ImageView mImageView, int image, int position) {
+        if (map[position] == value) {
+            mImageView.setImageResource(image);
+        }
+        return mImageView;
+    }
+
+//    private ImageView showPlayerImages(int[] map, ImageView mImageView, int position) {
+//        switch (map[position]){
+//            case -3:
+//                mImageView.setImageResource(imageid[2]);
+//                break;
+//            case -1:
+//                mImageView.setImageResource(imageid[26]);
+//                break;
+//            case 0:
+//                mImageView.setImageResource(imageid[0]);
+//                break;
+//            case 1:
+//                mImageView.setImageResource(imageid[0]);
+//                break;
+//            case 2:
+//                mImageView.setImageResource(imageid[5]);
+//                break;
+//            case 4:
+//                mImageView.setImageResource(imageid[7]);
+//                break;
+//            case 5:
+//                mImageView.setImageResource(imageid[13]);
+//                break;
+//            case 6:
+//                mImageView.setImageResource(imageid[3]);
+//                break;
+//            case 8:
+//                mImageView.setImageResource(imageid[9]);
+//                break;
+//            case 45:
+//                mImageView.setImageResource(imageid[11]);
+//                break;
+//            case 85:
+//                mImageView.setImageResource(imageid[12]);
+//                break;
+//            case 99:
+//                mImageView.setImageResource(imageid[1]);
+//                break;
+//            default:
+//        }
+//        return mImageView;
+//    }
 }
